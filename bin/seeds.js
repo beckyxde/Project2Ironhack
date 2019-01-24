452b6fea5b171e97d9ef0ec28d3cfa132870f5b7
Yelizaveta Crofts <yscrofts@hotmail.com>
2019-01-24 19:42:45 +0100

    some random model stuff no idea

--- bin/seeds.js
+++ bin/seeds.js
@@ -6,13 +6,13 @@ mongoose.connect(`mongodb://localhost/${dbName}`);
 
 const events = [
   {
-    genre : "jazz",
-    venue: {club: "wework", district: "Mitte"},
-    artist  : "Becky",
+    genre: "jazz",
+    venue: { club: "wework", district: "Mitte" },
+    artist: "Becky",
     date: "02.01.2019",
     ticket: true,
     price: 20
-   }]
+  }]
 
 event.create(events)
-.then(() => (console.log('success')))
+  .then(() => (console.log('success')))
