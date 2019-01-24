c612922bf852e8b044d5b7cc0b8373e128f8bb60
beckyxde <beckyxde@users.noreply.github.com>
2019-01-24 18:46:10 +0100

    signup/login

--- models/User.js
+++ models/User.js
@@ -1,21 +1,22 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const user_Schema = new Schema({
- name : {
-   type: String,
-   required: true
- },
- username : {
-   type: String,
-   required : true
- },
- password: {
-   type: String,
-   required: true
-}});
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  starred_events: [{ type : mongoose.Schema.Types.ObjectId, ref: 'Event' }]
+});
 
-const user = mongoose.model('user', user_Schema);
+const user = mongoose.model("user", user_Schema);
 
 module.exports = user;
