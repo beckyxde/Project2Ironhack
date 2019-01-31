4b96fa19e7b17eed8708b476d18d480b120ec205
beckyxde <beckyxde@users.noreply.github.com>
2019-01-31 18:22:34 +0100

    profile

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // /user
-router.get('/', (req, res, next) => {
-  //req.user <-- current user  
-  res.render('homepage');
+router.get("/", (req, res, next) => {
+  //req.user <-- current user
+  res.render("homepage");
 });
 
+// /user/search
+router.get("/search", (req, res, next) => {
+  //req.user <-- current user
+  res.render("search");
+});
 
 // /user/profile
-router.get('/search', (req, res, next) => {
-  //req.user <-- current user  
-  res.render('search');
+router.get("/profile", (req, res, next) => {
+  //req.user <-- current user
+  res.render("profile");
 });
 
 module.exports = router;
